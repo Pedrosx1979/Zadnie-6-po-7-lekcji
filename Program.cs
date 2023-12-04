47d88ef93ba03868c801a76f27d297eafdb81c96
Piotr Szydlak <pedrosx@o2.pl>
2023-12-04 11:30:59 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+Console.WriteLine("Podaj  swój wzrost: ");
+
+int height = Int32.Parse(Console.ReadLine());
+
+if (height <= 110)
+{
+    Console.WriteLine($"Jeżeli masz {height} Jeteś wielki jak Krasnolud ");
+}
+else if (height <= 150)
+{
+    Console.WriteLine($"Jeżeli masz {height} Jesteś jak Hobbit");
+}
+else if (height <= 180)
+{
+    Console.WriteLine($"Jeżeli masz {height} Jesteś Wielki");
+}
+else if (height > 180) 
+{
+    Console.WriteLine($"Jesteś olbrzymem");
+};
